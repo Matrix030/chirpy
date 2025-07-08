24a3c2f6d0277a6780e33b5b5be4135e61ed8f23
Matrix030 <rishigharat@gmail.com>
2025-07-08 18:28:36 -0400

    added method specifics

--- main.go
+++ main.go
@@ -7,24 +7,25 @@ import (
 	"sync/atomic"
 )
 
-//Create a struct in main.go that will hold any stateful, in-memory data we'll need to keep track of.
-//  In our case, we just need to keep track of the number of requests we've received.
+// Create a struct in main.go that will hold any stateful, in-memory data we'll need to keep track of.
+//
+//	In our case, we just need to keep track of the number of requests we've received.
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 	apiCfg := apiConfig{}
 
-
 	mux := http.NewServeMux()
 	/*Wrap the http.FileServer handler with the middleware method we just wrote.*/
 	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
-	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -40,8 +41,11 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
-/* Next, write a new middleware method on a *apiConfig that increments the fileserverHits counter 
- every time it's called.Here's the method signature I used:*/
+
+/*
+Next, write a new middleware method on a *apiConfig that increments the fileserverHits counter
+every time it's called.Here's the method signature I used:
+*/
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
@@ -49,8 +53,10 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
-/*Create a new handler that writes the number of requests that have been counted as plain text in this 
-format to the HTTP response:*/
+/*
+Create a new handler that writes the number of requests that have been counted as plain text in this
+format to the HTTP response:
+*/
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	count := cfg.fileserverHits.Load()
