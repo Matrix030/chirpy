b44fd79bdf2680f0618f2314f1a8afb5b4103a47
Matrix030 <rishigharat@gmail.com>
2025-07-07 22:38:11 -0400

    imma do some CUDA now

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
-	const port = "8080"
 	const filepathRoot = "."
+	const port = "8080"
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-	srv := http.Server{
+
+	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s \n", filepathRoot, port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
