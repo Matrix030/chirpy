b18c77b00ad58b56051b02a733a4d36cea67a2a8
Matrix030 <rishigharat@gmail.com>
2025-07-08 10:26:28 -0400

    more cleaner

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
