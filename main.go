e0f8cd3202e58379e3ac9ecd22fa0091b45df93a
Matrix030 <rishigharat@gmail.com>
2025-08-04 14:35:43 -0400

    clean up

--- main.go
+++ main.go
@@ -3,31 +3,24 @@ package main
 import (
 	"database/sql"
 	"github.com/Matrix030/chirpy/internal/database"
-	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
-	"time"
 )
 import _ "github.com/lib/pq"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
-}
-
-type User struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Email     string    `json:"email"`
+	env            string
 }
 
 func main() {
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	env := os.Getenv("PLATFORM")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatal("Failed to connect to DB:", err)
@@ -38,6 +31,7 @@ func main() {
 	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             queries,
+		env:            env,
 	}
 
 	const filepathRoot = "."
@@ -45,11 +39,14 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", handleChirp)
-	mux.HandleFunc("POST /api/users", apiCfg.handleCreateUser)
+
+	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUsers)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handleChirp)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
