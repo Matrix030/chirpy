7ddc2d437a015fc5b72aeeeecf93505f0f9d3e83
Matrix030 <rishigharat@gmail.com>
2025-08-05 11:48:49 -0400

    updated to drop table on reset

--- reset.go
+++ reset.go
@@ -1,23 +1,22 @@
 package main
 
-import (
-	"net/http"
-)
+import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	if cfg.env != "dev" {
-		http.Error(w, "forbidden in non-dev environment", http.StatusForbidden)
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
 		return
 	}
 
 	cfg.fileserverHits.Store(0)
-
-	err := cfg.db.DeleteAllUsers(r.Context())
+	err := cfg.db.Reset(r.Context())
 	if err != nil {
-		http.Error(w, "failed to reset users", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to reset the database: " + err.Error()))
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0 and users deleted"))
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
+
