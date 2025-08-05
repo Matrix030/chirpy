8137c568236ebaac65583237ae7c0ad4c38cf3ba
Matrix030 <rishigharat@gmail.com>
2025-08-05 11:49:15 -0400

    simple queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,13 +1,10 @@
 -- name: CreateUser :one
-insert into "users" (id, created_at, updated_at, email)
-values(
-	gen_random_uuid(),
-	NOW(),
-	NOW(),
-	$1
-	)
-returning *;
-
--- name: DeleteAllUsers :exec
-delete from users;
+INSERT INTO users (id, created_at, updated_at, email)
+VALUES (
+    gen_random_uuid(),
+    NOW(),
+    NOW(),
+    $1
+)
+RETURNING *;
 
